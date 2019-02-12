7d5d35e256a3157c961924b37ef99862748beef8
Rikosage <sageriko@gmail.com>
2019-02-12 18:18:10 +0300

    added facade

---
+++ src/WordifyFacade.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rikosage
+ * Date: 12/02/19
+ * Time: 18:12
+ */
+
+namespace rikosage\NumberWordify;
+
+use rikosage\NumberWordify\Unit\Currency\Rouble;
+
+/**
+ * Класс-обертка, для частоиспользуемых методов и форматов
+ * 
+ * @package rikosage\NumberWordify
+ */
+class WordifyFacade
+{
+    /**
+     * Возвращает число в виде строки прописью так, как обычно используется в документах.
+     *
+     * @param float $number
+     * @param string $unit
+     * @return string
+     */
+    public static function toDocumentInRoubles($number)
+    {
+        list($real, $float) = explode('.', sprintf("%0.2f", round((float)($number), 2)));
+
+        $formatter = new Formatter(new Rouble());
+        $result = $formatter->asWords($real);
+
+        $result .= " {$float} кп.";
+
+        return $result;
+    }
+}
